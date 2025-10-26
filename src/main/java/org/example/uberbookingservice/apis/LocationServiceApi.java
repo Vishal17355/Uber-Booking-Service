4fbfedd0e990ae9f3c16297939f66b0a3727d255
Vishal17355 <vishalkashyap73557@gmail.com>
2025-10-26 23:11:59 +0530

    configure eureka client and added to be discoverable by eureka

---
+++ src/main/java/org/example/uberbookingservice/apis/LocationServiceApi.java
@@ -0,0 +1,12 @@
+package org.example.uberbookingservice.apis;
+
+import org.example.uberbookingservice.DTO.DriverLocationDto;
+import org.example.uberbookingservice.DTO.NearbyDriverRequestDto;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface LocationServiceImpl {
+    @POST("/api/location/nearby/drivers")
+    Call<DriverLocationDto[]> getNearByDriver(@Body NearbyDriverRequestDto requestDto);
+}
